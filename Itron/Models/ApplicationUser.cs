3c383e445d23e83d8fd807f6e8fc60af93ac4da6
Szőke Viktor <viktor.szoke@dynflow.hu>
2021-10-29 16:18:32 +0200

    Add project files.

---
+++ Itron/Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Itron.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
